7f6c2e9477b8cb6901258ad72423c61da8bbbda7
Al-baqer Ameen Abbas <baqerabbas@aas.com.sa>
2020-12-28 15:03:52 +0300

    thired update

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.get('/', (req, res)=> {
     if(process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, 'public')));
       app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-        // res.sendFile(path.join(__dirname, 'public',  'index.html'));
+        // res.sendFile(path.join(__dirname, 'public/index.html'));
+         res.sendFile(path.join(__dirname, 'public',  'index.html'));
         });
     }
 
